79ff8e930655027e83663972b598473501ebb63a
AntonWiklund1 <awiklund76@gmail.com>
2024-05-01 20:18:33 +0300

    Refactor main.py to include DeiT model, train function, and constants module

---
+++ main.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet50
+from model.model import DeiT
+import constants
+import train.train as train
+from dataset import ImageDataset
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+from train.train import train
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+image_size = constants.image_size
+patch_size = constants.patch_size
+num_classes = constants.num_classes
+embed_dim = constants.embed_dim
+T = constants.T
+
+
+def main():
+    model = DeiT(image_size, patch_size, num_classes, embed_dim).to(device)
+    teacher_model = resnet50(pretrained=True).to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+
+
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize images to the input size expected by the model
+        transforms.ToTensor(),          # Convert images to PyTorch tensors
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize with ImageNet stats
+    ])
+
+    csv_file = "./data/train.csv"
+    dataset = ImageDataset(csv_file=csv_file, transform=transform)
+    data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    # Assuming data_loader is defined
+    train(model, data_loader, teacher_model, criterion, optimizer, device)
+
+
+if __name__ == "__main__":
+    main()
