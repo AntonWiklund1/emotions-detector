6a12db2e80cc56733b0098085aedad418793ae72
AntonWiklund1 <awiklund76@gmail.com>
2024-05-01 20:24:28 +0300

    Add batch_size constant to constants/__init__.py

--- constants/__init__.py
+++ constants/__init__.py
@@ -5,3 +5,4 @@ embed_dim = 768
 T = 2  # Temperature for distillation
 lambda_coeff = 0.5  # Balance coefficient
 num_epochs = 10  # Number of epochs for training
+batch_size = 32  # Batch size for training
