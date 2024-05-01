750fa186d87cc42017a114f3f9c4045d70fc7027
AntonWiklund1 <awiklund76@gmail.com>
2024-05-01 20:18:06 +0300

    Add constants module with initial values

---
+++ constants/__init__.py
@@ -0,0 +1,7 @@
+image_size = 224
+patch_size = 16
+num_classes = 1000
+embed_dim = 768
+T = 2  # Temperature for distillation
+lambda_coeff = 0.5  # Balance coefficient
+num_epochs = 10  # Number of epochs for training
