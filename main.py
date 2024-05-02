f27bfb4b9d5a4004664d9e3ae94d8c42173cb20f
AntonWiklund1 <awiklund76@gmail.com>
2024-05-03 01:25:21 +0300

    Update main.py with load_checkpoint function for model checkpoint loading

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from torch.utils.data import random_split
 from transformers import AutoImageProcessor, AutoModelForImageClassification
 from evaluation import test, train_and_validate, validate
 from model.losses import DistillationLoss
-
+from evaluation.train import load_checkpoint
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -36,18 +36,19 @@ def main():
     processor = AutoImageProcessor.from_pretrained("ycbq999/facial_emotions_image_detection")
     teacher_model = AutoModelForImageClassification.from_pretrained("ycbq999/facial_emotions_image_detection").to(device)
 
-
     # Load the models checkpoint if it matches the model architecture
-    try:
-        model.load_state_dict(torch.load("best_model.pth"))
-        print("Loaded model checkpoint")
-    except RuntimeError:
-        print("Model checkpoint does not match the model architecture. Training the model from scratch")
+    
     
 
     base_criterion = nn.CrossEntropyLoss()
     criterion = DistillationLoss(base_criterion, teacher_model, 'soft', lambda_coeff, T)
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+
+    try:
+        model, optimizer, best_val_loss = load_checkpoint(model, optimizer, device)
+        print("Loaded model checkpoint")
+    except RuntimeError:
+        print("Model checkpoint does not match the model architecture. Training the model from scratch")
 
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -56,18 +57,18 @@ def main():
     ])
 
     dataset_csv_file = "./data/train.csv"  # Single dataset file
-    full_dataset = ImageDataset(csv_file=dataset_csv_file, transform=transform, rows=5000, processor=processor)
+    full_dataset = ImageDataset(csv_file=dataset_csv_file, processor=processor)
 
     # Split dataset into train and validation
     train_size = int(0.8 * len(full_dataset))  # 80% of the dataset for training
     val_size = len(full_dataset) - train_size  # Remaining 20% for validation
     train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
 
-    train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
     val_data_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     # Train and validate the model
-    train_and_validate(model, train_data_loader, val_data_loader, teacher_model, criterion, optimizer, device)
+    train_and_validate(model, train_data_loader, val_data_loader, teacher_model, criterion, optimizer, device, best_val_loss)
 
     # Test the model
     test_csv_file = "./data/test_with_emotions.csv"
