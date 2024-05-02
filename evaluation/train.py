bdccb26abc3a6785db11957d417e220d4680f01f
AntonWiklund1 <awiklund76@gmail.com>
2024-05-02 20:53:38 +0300

    Update train.py to include tensorboard logging for training and validation loss

--- evaluation/train.py
+++ evaluation/train.py
@@ -2,6 +2,13 @@ import torch
 import torch.nn as nn
 import constants
 from evaluation.validate import validate
+from torch.utils.tensorboard import SummaryWriter
+
+import datetime
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+
+writer = SummaryWriter(f'runs/ed_{current_time}')
 
 lambda_coeff = constants.lambda_coeff
 T = constants.T
@@ -10,35 +17,34 @@ torch.manual_seed(42)
 
 def train_and_validate(model, data_loader, val_loader, teacher_model, criterion, optimizer, device):
     best_val_loss = float('inf')  # Initialize the best validation loss to infinity
-    print(f"Training the model on {device} ")
     for epoch in range(num_epochs):
         model.train()
-        teacher_model.eval()
+        teacher_model.eval()  # Teacher in no-grad mode
 
-        for images, labels in data_loader:
+        for batch_idx, (images, labels) in enumerate(data_loader):
             images, labels = images.to(device), labels.to(device)
-            
-            # Forward pass
-            class_logits, dist_logits = model(images)
-            with torch.no_grad():
-                teacher_logits = teacher_model(images)
-            
-            # Calculate losses
-            loss_class = criterion(class_logits, labels)
-            loss_distill = criterion(dist_logits, torch.softmax(teacher_logits / T, dim=-1))
-            loss = loss_class + lambda_coeff * loss_distill
 
-            # Backward and optimize
+            # Forward pass through the student model
+            class_logits, dist_logits = model(images)
+
+            # Ensure to pass 'images' as 'inputs' to the DistillationLoss
+            loss = criterion(images, (class_logits, dist_logits), labels)
+
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        # Validation step after each epoch
-        val_loss, val_accuracy = validate(model, val_loader, criterion, device)
+            # Add to tensorboard
+            writer.add_scalar('Training loss', loss.item(), epoch * len(data_loader) + batch_idx)
+
+        # Validation after each epoch
+        val_loss, val_accuracy = validate(model, teacher_model, val_loader, criterion, device)
+        writer.add_scalar('Validation loss', val_loss, epoch)
         print(f"Epoch {epoch}: Training Loss: {loss.item()}, Validation Loss: {val_loss}, Validation Accuracy: {val_accuracy}%")
 
-        # Save the best model
+        # Save best model
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             torch.save(model.state_dict(), "best_model.pth")
             print("Saved better model with validation loss:", val_loss)
+
