b849912fa274e6cb1cc37f6355d6473ad3c6741f
AntonWiklund1 <awiklund76@gmail.com>
2024-05-03 01:25:35 +0300

    Refactor evaluation module to include save_checkpoint and load_checkpoint functions

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,3 +1,3 @@
-from evaluation.train import train_and_validate
+from evaluation.train import train_and_validate, save_checkpoint, load_checkpoint
 from evaluation.test import test
 from evaluation.validate import validate
