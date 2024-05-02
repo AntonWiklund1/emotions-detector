5eb0814a30f78de1997714d214b8ec1c2e0ba423
AntonWiklund1 <awiklund76@gmail.com>
2024-05-03 01:25:31 +0300

    Update batch_size to 128 in constants/__init__.py

--- constants/__init__.py
+++ constants/__init__.py
@@ -5,5 +5,5 @@ embed_dim = 768
 T = 2  # Temperature for distillation
 lambda_coeff = 0.1  # Coefficient for distillation loss
 num_epochs = 50  # Number of epochs for training
-batch_size = 64  # Batch size for training
+batch_size = 128  # Batch size for training
 lr = 0.001
