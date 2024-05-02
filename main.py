e2e940abaa541d644a91276a0ec2ec21731a6cef
AntonWiklund1 <awiklund76@gmail.com>
2024-05-02 13:44:25 +0300

    using validation now

--- main.py
+++ main.py
@@ -3,13 +3,16 @@ import torch.nn as nn
 from torchvision.models import resnet50, ResNet50_Weights
 from model.model import DeiT
 import constants
-import train.train as train
+import evaluation.train as train
 from dataset import ImageDataset
 from torchvision import transforms
 from torch.utils.data import DataLoader
+from torch.utils.data import random_split
 
-from train.train import train
-from train.test import test
+from evaluation import test, train_and_validate, validate
+
+import warnings
+warnings.filterwarnings("ignore")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 image_size = constants.image_size
@@ -28,27 +31,32 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
 
-
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),  # Resize images to the input size expected by the model
-        transforms.ToTensor(),          # Convert images to PyTorch tensors
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize with ImageNet stats
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    # Train the model
-    csv_file = "./data/train.csv"
-    dataset = ImageDataset(csv_file=csv_file, transform=transform)
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    dataset_csv_file = "./data/train.csv"  # Single dataset file
+    full_dataset = ImageDataset(csv_file=dataset_csv_file, transform=transform)
 
-    train(model, data_loader, teacher_model, criterion, optimizer, device)
+    # Split dataset into train and validation
+    train_size = int(0.8 * len(full_dataset))  # 80% of the dataset for training
+    val_size = len(full_dataset) - train_size  # Remaining 20% for validation
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+
+    train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_data_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    # Train and validate the model
+    train_and_validate(model, train_data_loader, val_data_loader, teacher_model, criterion, optimizer, device)
 
     # Test the model
     test_csv_file = "./data/test_with_emotions.csv"
     test_dataset = ImageDataset(csv_file=test_csv_file, transform=transform)
     test_data_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
-
     test(model, test_data_loader)
-
 
 if __name__ == "__main__":
     main()
+
