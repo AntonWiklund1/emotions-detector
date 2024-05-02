eff2ea3e242665a5bdb920da1c9bb5e6d9238fdf
AntonWiklund1 <awiklund76@gmail.com>
2024-05-02 14:41:01 +0300

    Update constants/__init__.py with new values for num_classes and lambda_coeff

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,8 +1,8 @@
 image_size = 224
 patch_size = 16
-num_classes = 1000
+num_classes = 7
 embed_dim = 768
 T = 2  # Temperature for distillation
-lambda_coeff = 0.5  # Coefficient for distillation loss
+lambda_coeff = 0.1  # Coefficient for distillation loss
 num_epochs = 50  # Number of epochs for training
 batch_size = 64  # Batch size for training
