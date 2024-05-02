608aad93efdf7d15c43acce47f4cfde84a2db0b5
AntonWiklund1 <awiklund76@gmail.com>
2024-05-02 20:53:29 +0300

    Update main.py with new imports and loss function for knowledge distillation

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from dataset import ImageDataset
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
-
-
-
+from transformers import AutoImageProcessor, AutoModelForImageClassification
 from evaluation import test, train_and_validate, validate
+from model.losses import DistillationLoss
 
 import warnings
 warnings.filterwarnings("ignore")
@@ -23,17 +22,32 @@ num_classes = constants.num_classes
 embed_dim = constants.embed_dim
 T = constants.T
 batch_size = constants.batch_size
+lr = constants.lr
+lambda_coeff = constants.lambda_coeff
 
 #SET SEED
 torch.manual_seed(42)
 
 def main():
     model = DeiT(image_size, patch_size, num_classes, embed_dim).to(device)
-    teacher_model = resnet50(weights=ResNet50_Weights.DEFAULT).to(device)
-    teacher_model.fc = nn.Linear(in_features=teacher_model.fc.in_features, out_features=num_classes).to(device)
+    # teacher_model = resnet50(weights=ResNet50_Weights.DEFAULT).to(device)
+    # teacher_model.fc = nn.Linear(in_features=teacher_model.fc.in_features, out_features=num_classes).to(device)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
+    processor = AutoImageProcessor.from_pretrained("ycbq999/facial_emotions_image_detection")
+    teacher_model = AutoModelForImageClassification.from_pretrained("ycbq999/facial_emotions_image_detection").to(device)
+
+
+    # Load the models checkpoint if it matches the model architecture
+    try:
+        model.load_state_dict(torch.load("best_model.pth"))
+        print("Loaded model checkpoint")
+    except RuntimeError:
+        print("Model checkpoint does not match the model architecture. Training the model from scratch")
+    
+
+    base_criterion = nn.CrossEntropyLoss()
+    criterion = DistillationLoss(base_criterion, teacher_model, 'soft', lambda_coeff, T)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
 
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -42,7 +56,7 @@ def main():
     ])
 
     dataset_csv_file = "./data/train.csv"  # Single dataset file
-    full_dataset = ImageDataset(csv_file=dataset_csv_file, transform=transform)
+    full_dataset = ImageDataset(csv_file=dataset_csv_file, transform=transform, rows=5000, processor=processor)
 
     # Split dataset into train and validation
     train_size = int(0.8 * len(full_dataset))  # 80% of the dataset for training
