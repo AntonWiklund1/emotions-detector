7f92e03da820e2f211bd2aacfb27d58f12714fd3
AntonWiklund1 <awiklund76@gmail.com>
2024-05-02 20:53:12 +0300

    Update constants/__init__.py with new values for batch_size and lr

--- constants/__init__.py
+++ constants/__init__.py
@@ -6,3 +6,4 @@ T = 2  # Temperature for distillation
 lambda_coeff = 0.1  # Coefficient for distillation loss
 num_epochs = 50  # Number of epochs for training
 batch_size = 64  # Batch size for training
+lr = 0.001
