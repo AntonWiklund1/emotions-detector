a5e813eb2373b7143a8e95354af0e7c0b44818bb
AntonWiklund1 <awiklund76@gmail.com>
2024-05-15 18:10:11 +0300

    67% accuracy

--- resnet/res_model.py
+++ resnet/res_model.py
@@ -20,14 +20,15 @@ class block(nn.Module):
         self.expansion = 4
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.cbam1 = CBAM(out_channels)
+        
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.cbam2 = CBAM(out_channels)
+        
         self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
         self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
         self.relu = nn.ReLU()
         self.identity_downsample = identity_downsample
+        #self.cbam = CBAM(out_channels * self.expansion)
 
     def forward(self, x):
         identity = x
@@ -38,7 +39,6 @@ class block(nn.Module):
 
         x = self.conv2(x)
         x = self.bn2(x)
-        x = self.cbam2(x)
         x = self.relu(x)
 
         x = self.conv3(x)
@@ -48,6 +48,7 @@ class block(nn.Module):
             identity = self.identity_downsample(identity)
 
         x += identity
+        #x = self.cbam(x)
         x = self.relu(x)
 
         return x
@@ -65,27 +66,27 @@ class ResNet(nn.Module):
     """
     def __init__(self, block, layers, image_channels, num_classes):
         super(ResNet, self).__init__()
-        self.in_channels = 16  # Initial number of channels after the first convolution layer
+        self.in_channels = 64  # Initial number of channels after the first convolution layer
         # Initial convolutional layer before the first block
-        self.conv1 = nn.Conv2d(image_channels, 16, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
+        self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU()
-        #self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         # ResNet layers: each layer is a sequence of blocks
-        self.layer1 = self._make_layer(block, layers[0], out_channels=16, stride=1)
-        self.layer2 = self._make_layer(block, layers[1], out_channels=32, stride=2)
-        self.layer3 = self._make_layer(block, layers[2], out_channels=64, stride=2)
-        self.layer4 = self._make_layer(block, layers[3], out_channels=128, stride=2)  
+        self.layer1 = self._make_layer(block, layers[0], out_channels=64, stride=1)
+        self.layer2 = self._make_layer(block, layers[1], out_channels=128, stride=2)
+        self.layer3 = self._make_layer(block, layers[2], out_channels=256, stride=2)
+        self.layer4 = self._make_layer(block, layers[3], out_channels=512, stride=2)  
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(128 * 4, num_classes)  # Final fully connected layer
+        self.fc = nn.Linear(512 * 4, num_classes)  # Final fully connected layer
 
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        #x = self.maxpool(x)
+        x = self.maxpool(x)
 
         # Passing the input through all the residual layers
         x = self.layer1(x)
@@ -120,48 +121,5 @@ class ResNet(nn.Module):
 
         return nn.Sequential(*layers)
 
-class ChannelAttention(nn.Module):
-    def __init__(self, in_channels, reduction_ratio=16):
-        super(ChannelAttention, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Conv2d(in_channels, in_channels // reduction_ratio, 1, bias=False),
-            nn.ReLU(),
-            nn.Conv2d(in_channels // reduction_ratio, in_channels, 1, bias=False)
-        )
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg_out = self.fc(self.avg_pool(x))
-        max_out = self.fc(self.max_pool(x))
-        out = avg_out + max_out
-        return self.sigmoid(out)
-
-class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size=7):
-        super(SpatialAttention, self).__init__()
-        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg_out = torch.mean(x, dim=1, keepdim=True)
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv1(x)
-        return self.sigmoid(x)
-
-class CBAM(nn.Module):
-    def __init__(self, in_channels, reduction_ratio=16, kernel_size=7):
-        super(CBAM, self).__init__()
-        self.channel_attention = ChannelAttention(in_channels, reduction_ratio)
-        self.spatial_attention = SpatialAttention(kernel_size)
-
-    def forward(self, x):
-        x = x * self.channel_attention(x)
-        x = x * self.spatial_attention(x)
-        return x
-
-
 def resnet(img_channels=1, num_classes=7):
-    return ResNet(block, [3, 4, 23, 3], img_channels, num_classes)
+    return ResNet(block, [3, 4, 6, 3], img_channels, num_classes)
