89002aafa053a59b7b55adaeed21ee059079439a
AntonWiklund1 <awiklund76@gmail.com>
2024-05-19 23:18:03 +0300

    refactor: Improve CBAM module implementation

--- resnet/cbam.py
+++ resnet/cbam.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class CBAM(nn.Module):
-
+    """Implementation of Convolutional Block Attention Module (CBAM) as described in Woo et al., 2018"""
     def __init__(self, n_channels_in, reduction_ratio=16, kernel_size=7):
         super(CBAM, self).__init__()
         self.n_channels_in = n_channels_in
@@ -15,46 +15,10 @@ class CBAM(nn.Module):
 
     def forward(self, f):
         chan_att = self.channel_attention(f)
-        # print(chan_att.size())
         fp = chan_att * f
-        # print(fp.size())
         spat_att = self.spatial_attention(fp)
-        # print(spat_att.size())
         fpp = spat_att * fp
-        # print(fpp.size())
         return fpp
-
-
-class SpatialAttention(nn.Module):
-    def __init__(self, kernel_size):
-        super(SpatialAttention, self).__init__()
-        self.kernel_size = kernel_size
-
-        assert kernel_size % 2 == 1, "Odd kernel size required"
-        self.conv = nn.Conv2d(in_channels = 2, out_channels = 1, kernel_size = kernel_size, padding= int((kernel_size-1)/2))
-        # batchnorm
-
-    def forward(self, x):
-        max_pool = self.agg_channel(x, "max")
-        avg_pool = self.agg_channel(x, "avg")
-        pool = torch.cat([max_pool, avg_pool], dim = 1)
-        conv = self.conv(pool)
-        # batchnorm ????????????????????????????????????????????
-        conv = conv.repeat(1,x.size()[1],1,1)
-        att = torch.sigmoid(conv)        
-        return att
-
-    def agg_channel(self, x, pool = "max"):
-        b,c,h,w = x.size()
-        x = x.view(b, c, h*w)
-        x = x.permute(0,2,1)
-        if pool == "max":
-            x = F.max_pool1d(x,c)
-        elif pool == "avg":
-            x = F.avg_pool1d(x,c)
-        x = x.permute(0,2,1)
-        x = x.view(b,1,h,w)
-        return x
 
 
 class ChannelAttention(nn.Module):
@@ -62,7 +26,7 @@ class ChannelAttention(nn.Module):
         super(ChannelAttention, self).__init__()
         self.n_channels_in = n_channels_in
         self.reduction_ratio = reduction_ratio
-        self.middle_layer_size = int(self.n_channels_in/ float(self.reduction_ratio))
+        self.middle_layer_size = max(1, self.n_channels_in // self.reduction_ratio)
 
         self.bottleneck = nn.Sequential(
             nn.Linear(self.n_channels_in, self.middle_layer_size),
@@ -70,24 +34,28 @@ class ChannelAttention(nn.Module):
             nn.Linear(self.middle_layer_size, self.n_channels_in)
         )
 
-
     def forward(self, x):
-        kernel = (x.size()[2], x.size()[3])
-        avg_pool = F.avg_pool2d(x, kernel )
-        max_pool = F.max_pool2d(x, kernel)
-
-        
-        avg_pool = avg_pool.view(avg_pool.size()[0], -1)
-        max_pool = max_pool.view(max_pool.size()[0], -1)
-        
+        avg_pool = F.adaptive_avg_pool2d(x, (1, 1)).view(x.size(0), -1)
+        max_pool = F.adaptive_max_pool2d(x, (1, 1)).view(x.size(0), -1)
 
         avg_pool_bck = self.bottleneck(avg_pool)
         max_pool_bck = self.bottleneck(max_pool)
 
         pool_sum = avg_pool_bck + max_pool_bck
+        sig_pool = torch.sigmoid(pool_sum).unsqueeze(2).unsqueeze(3)
 
-        sig_pool = torch.sigmoid(pool_sum)
-        sig_pool = sig_pool.unsqueeze(2).unsqueeze(3)
+        return sig_pool
 
-        out = sig_pool.repeat(1,1,kernel[0], kernel[1])
-        return out
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size):
+        super(SpatialAttention, self).__init__()
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=(kernel_size - 1) // 2, bias=False)
+
+    def forward(self, x):
+        max_pool, _ = torch.max(x, dim=1, keepdim=True)
+        avg_pool = torch.mean(x, dim=1, keepdim=True)
+        pool = torch.cat([max_pool, avg_pool], dim=1)
+        conv = self.conv(pool)
+        att = torch.sigmoid(conv)
+        return att
