fff49cce3cedd1c7c433deb920a8f8d0f060bcc3
AntonWiklund1 <awiklund76@gmail.com>
2024-05-19 23:17:54 +0300

    refactor: Reset index in ImageDataset to avoid out-of-bounds issues

--- dataset/ImageDataset.py
+++ dataset/ImageDataset.py
@@ -8,38 +8,27 @@ torch.manual_seed(42)
 
 class ImageDataset(Dataset):
     def __init__(self, csv_file, transform=None, rows=None):
-        """
-        Initialize the dataset.
-
-        Args:
-            csv_file (str): Path to the CSV file containing image data and labels.
-            transform (callable, optional): Optional transform to be applied on each image.
-            rows (int, optional): Number of rows to read from the CSV file. Useful for debugging or reduced dataset training.
-        """
         self.data_frame = pd.read_csv(csv_file, nrows=rows)
         self.transform = transform
 
         # Filter out black or corrupted images
         self.data_frame = self.data_frame[self.data_frame['pixels'].apply(self.is_valid_image)]
-        
+        self.data_frame.reset_index(drop=True, inplace=True)  # Reset index to avoid any out-of-bounds issues
+
     def __len__(self):
-        """Return the number of items in the dataset."""
         return len(self.data_frame)
 
     def __getitem__(self, idx):
-        # Retrieve the pixel string and convert it into numpy array of integers
+        if idx >= len(self.data_frame):
+            raise IndexError("Index out of bounds")
+        
         img_pixel_str = self.data_frame.iloc[idx]['pixels']
         pixels = np.array(img_pixel_str.split(), dtype=np.uint8).reshape(48, 48)
-        image = Image.fromarray(pixels, 'L')  # Create a PIL image in grayscale ('L' mode)
+        image = Image.fromarray(pixels, 'L')
 
-        # Convert grayscale to RGB
-        # image = image.convert('RGB')
-        
-        # Apply the transform if it is specified
         if self.transform:
             image = self.transform(image)
 
-        # Retrieve the label and convert it to integer
         label = int(self.data_frame.iloc[idx]['emotion'])
         return image, label
     
