e991a24e566736a45d1c6848a9b9cb11545f8e99
AntonWiklund1 <awiklund76@gmail.com>
2024-05-22 23:11:33 +0300

    refactor: Update ResNeXtBottleneck initialization to include bottleneck_width parameter

--- resnet/ResNeXt.py
+++ resnet/ResNeXt.py
@@ -1,24 +1,26 @@
 import torch
 import torch.nn as nn
 from .cbam import CBAM
+import math
 
 torch.manual_seed(42)
 
-class ResNeXtBlock(nn.Module):
+class ResNeXtBottleneck(nn.Module):
     expansion = 4
-    def __init__(self, in_channels, out_channels, cardinality=32, identity_downsample=None, stride=1):
-        super(ResNeXtBlock, self).__init__()
-        self.cardinality = cardinality
-        D = out_channels * self.cardinality // 32
-        self.conv1 = nn.Conv2d(in_channels, D, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn1 = nn.BatchNorm2d(D, momentum=0.01)
-        
-        self.conv2 = nn.Conv2d(D, D, kernel_size=3, stride=stride, padding=1, groups=self.cardinality, bias=False)
-        self.bn2 = nn.BatchNorm2d(D, momentum=0.01)
-        
-        self.conv3 = nn.Conv2d(D, out_channels * self.expansion, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion, momentum=0.01)
-        
+    def __init__(self, in_channels, out_channels, cardinality=32, bottleneck_width=4, identity_downsample=None, stride=1):
+        super(ResNeXtBottleneck, self).__init__()
+        D = int(math.floor(out_channels * (bottleneck_width / 64.0)))
+        C = cardinality
+
+        self.conv1 = nn.Conv2d(in_channels, D * C, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(D * C)
+
+        self.conv2 = nn.Conv2d(D * C, D * C, kernel_size=3, stride=stride, padding=1, groups=C, bias=False)
+        self.bn2 = nn.BatchNorm2d(D * C)
+
+        self.conv3 = nn.Conv2d(D * C, out_channels * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels * self.expansion)
+
         self.relu = nn.ReLU(inplace=True)
         self.identity_downsample = identity_downsample
 
@@ -30,27 +32,26 @@ class ResNeXtBlock(nn.Module):
     def forward(self, x):
         identity = x
 
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
 
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu(x)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
 
-        x = self.conv3(x)
-        x = self.bn3(x)
+        out = self.conv3(out)
+        out = self.bn3(out)
 
-        x = self.cbam(x)
-        
+        out = self.cbam(out)
+
         if self.identity_downsample is not None:
             identity = self.identity_downsample(identity)
 
-        x += identity
-        
-        x = self.relu(x)
+        out += identity
+        out = self.relu(out)
 
-        return x
+        return out
 
     def _initialize_weights(self):
         for m in self.modules():
@@ -63,12 +64,14 @@ class ResNeXtBlock(nn.Module):
                 nn.init.zeros_(m.bias)
 
 class ResNeXt(nn.Module):
-    def __init__(self, block, layers, image_channels, num_classes, cardinality=32):
+    def __init__(self, block, layers, image_channels=1, num_classes=7, cardinality=32, bottleneck_width=4):
         super(ResNeXt, self).__init__()
         self.in_channels = 64
         self.cardinality = cardinality
+        self.bottleneck_width = bottleneck_width
+
         self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64, momentum=0.01)
+        self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
@@ -78,6 +81,9 @@ class ResNeXt(nn.Module):
         self.layer4 = self._make_layer(block, layers[3], out_channels=512, stride=2)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.dropout = nn.Dropout(p=0.5)
+
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         # Initialize weights
@@ -95,26 +101,27 @@ class ResNeXt(nn.Module):
         x = self.layer4(x)
 
         x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
         x = self.fc(x)
 
         return x
 
-    def _make_layer(self, block, num_residual_blocks, out_channels, stride):
+    def _make_layer(self, block, num_blocks, out_channels, stride):
         identity_downsample = None
         layers = []
 
         if stride != 1 or self.in_channels != out_channels * block.expansion:
             identity_downsample = nn.Sequential(
                 nn.Conv2d(self.in_channels, out_channels * block.expansion, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(out_channels * block.expansion, momentum=0.01),
+                nn.BatchNorm2d(out_channels * block.expansion),
             )
 
-        layers.append(block(self.in_channels, out_channels, self.cardinality, identity_downsample, stride))
+        layers.append(block(self.in_channels, out_channels, self.cardinality, self.bottleneck_width, identity_downsample, stride))
         self.in_channels = out_channels * block.expansion
 
-        for _ in range(num_residual_blocks - 1):
-            layers.append(block(self.in_channels, out_channels, self.cardinality))
+        for _ in range(1, num_blocks):
+            layers.append(block(self.in_channels, out_channels, self.cardinality, self.bottleneck_width))
 
         return nn.Sequential(*layers)
 
@@ -131,8 +138,8 @@ class ResNeXt(nn.Module):
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
                 nn.init.zeros_(m.bias)
 
-def resnext50(img_channels=1, num_classes=7, cardinality=32):
-    return ResNeXt(ResNeXtBlock, [3, 4, 6, 3], img_channels, num_classes, cardinality)
+def resnext50(img_channels=1, num_classes=7):
+    return ResNeXt(ResNeXtBottleneck, [3, 4, 6, 3], img_channels, num_classes)
 
-def resnext101(img_channels=1, num_classes=7, cardinality=32):
-    return ResNeXt(ResNeXtBlock, [3, 4, 23, 3], img_channels, num_classes, cardinality)
+def resnext101(img_channels=1, num_classes=7):
+    return ResNeXt(ResNeXtBottleneck, [3, 4, 23, 3], img_channels, num_classes)
