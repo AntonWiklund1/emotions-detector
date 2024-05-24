fc1722a9ced97066ce62b3f140c642b201345b63
AntonWiklund1 <awiklund76@gmail.com>
2024-05-24 22:34:52 +0300

    refactor: Update ResNet implementation to support different activation functions

--- resnet/resnet.py
+++ resnet/resnet.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
-from .cbam import CBAM
+from model.cbam import CBAM
 
 torch.manual_seed(42)
 
 class block(nn.Module):
-    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1, activation='relu'):
         super(block, self).__init__()
         self.expansion = 4
         self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
@@ -17,10 +17,15 @@ class block(nn.Module):
         self.conv3 = nn.Conv2d(out_channels, out_channels * self.expansion, kernel_size=1, stride=1, padding=0)
         self.bn3 = nn.BatchNorm2d(out_channels * self.expansion, momentum=0.01)
         
-        self.relu = nn.ReLU()
+        
+        if activation == 'relu':
+            self.activation = nn.ReLU()
+        elif activation == 'leaky_relu':
+            self.activation = nn.LeakyReLU()
+        elif activation == 'prelu':
+            self.activation = nn.PReLU()
+        
         self.identity_downsample = identity_downsample
-
-       
 
         # Initialize weights
         self._initialize_weights()
@@ -30,11 +35,11 @@ class block(nn.Module):
 
         x = self.conv1(x)
         x = self.bn1(x)
-        x = self.relu(x)
+        x = self.activation(x)
 
         x = self.conv2(x)
         x = self.bn2(x)
-        x = self.relu(x)
+        x = self.activation(x)
 
         x = self.conv3(x)
         x = self.bn3(x)
@@ -44,7 +49,7 @@ class block(nn.Module):
 
         x += identity
         
-        x = self.relu(x)
+        x = self.activation(x)
 
         return x
 
@@ -59,7 +64,7 @@ class block(nn.Module):
                 nn.init.zeros_(m.bias)
 
 class ResNet(nn.Module):
-    def __init__(self, block, layers, image_channels, num_classes):
+    def __init__(self, block, layers, image_channels, num_classes, activation='relu'):
         super(ResNet, self).__init__()
         self.in_channels = 64
         self.conv1 = nn.Conv2d(image_channels, 64, kernel_size=7, stride=2, padding=3)
@@ -67,10 +72,10 @@ class ResNet(nn.Module):
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.layer1 = self._make_layer(block, layers[0], out_channels=64, stride=1)
-        self.layer2 = self._make_layer(block, layers[1], out_channels=128, stride=2)
-        self.layer3 = self._make_layer(block, layers[2], out_channels=256, stride=2)
-        self.layer4 = self._make_layer(block, layers[3], out_channels=512, stride=2)
+        self.layer1 = self._make_layer(block, layers[0], out_channels=64, stride=1, activation=activation)
+        self.layer2 = self._make_layer(block, layers[1], out_channels=128, stride=2, activation=activation)
+        self.layer3 = self._make_layer(block, layers[2], out_channels=256, stride=2, activation=activation)
+        self.layer4 = self._make_layer(block, layers[3], out_channels=512, stride=2, activation=activation)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * 4, num_classes)
@@ -95,7 +100,7 @@ class ResNet(nn.Module):
 
         return x
 
-    def _make_layer(self, block, num_residual_blocks, out_channels, stride):
+    def _make_layer(self, block, num_residual_blocks, out_channels, stride, activation='relu'):
         identity_downsample = None
         layers = []
 
@@ -105,11 +110,11 @@ class ResNet(nn.Module):
                 nn.BatchNorm2d(out_channels * 4, momentum=0.01),
             )
 
-        layers.append(block(self.in_channels, out_channels, identity_downsample, stride))
+        layers.append(block(self.in_channels, out_channels, identity_downsample, stride, activation=activation))
         self.in_channels = out_channels * 4
 
         for _ in range(num_residual_blocks - 1):
-            layers.append(block(self.in_channels, out_channels))
+            layers.append(block(self.in_channels, out_channels, activation=activation))
 
         return nn.Sequential(*layers)
 
@@ -126,8 +131,8 @@ class ResNet(nn.Module):
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
                 nn.init.zeros_(m.bias)
 
-def resnet50(img_channels=1, num_classes=7):
-    return ResNet(block, [3, 4, 6, 3], img_channels, num_classes)
+def own_resnet50(img_channels=1, num_classes=7, activation='relu'):
+    return ResNet(block, [3, 4, 6, 3], img_channels, num_classes, activation=activation)
 
-def resnet100(img_channels=1, num_classes=7):
+def own_resnet100(img_channels=1, num_classes=7):
     return ResNet(block, [3, 4, 23, 3], img_channels, num_classes)
