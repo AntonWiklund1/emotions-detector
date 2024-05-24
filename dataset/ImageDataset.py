24b127adf7618327be4ddf40ab1f64699973e51c
AntonWiklund1 <awiklund76@gmail.com>
2024-05-24 22:33:36 +0300

    refactor: Add face detection to ImageDataset

--- dataset/ImageDataset.py
+++ dataset/ImageDataset.py
@@ -4,27 +4,31 @@ import numpy as np
 from PIL import Image
 import torch
 import hashlib
+import face_recognition
 
 torch.manual_seed(42)
 
 class ImageDataset(Dataset):
-    def __init__(self, csv_file, transform=None, rows=None):
-        # Handle CSV with or without index column
+    """Custom dataset for loading images from CSV files with pixel values and emotion labels."""
+    def __init__(self,csv_file, fix=None, transform=None, rows=None):
         self.data_frame = pd.read_csv(csv_file, nrows=rows)
         
-        # If the CSV has an extra index column, drop it
+        print(f"Loaded {len(self.data_frame)} images from {csv_file}")
+        
         if self.data_frame.columns[0] == '':
             self.data_frame = self.data_frame.drop(self.data_frame.columns[0], axis=1)
         
         self.transform = transform
+        self.fix = fix
+        
+        if self.fix:
+            self.data_frame = self.data_frame[self.data_frame['pixels'].apply(self.is_valid_image)]
+            self.data_frame = self.data_frame[self.data_frame['pixels'].apply(self.contains_face)]
+            self.data_frame = self.remove_duplicate_images(self.data_frame)
+            print(f"Filtered to {len(self.data_frame)} images")
 
-        # Filter out black or corrupted images
-        self.data_frame = self.data_frame[self.data_frame['pixels'].apply(self.is_valid_image)]
-        
-        # Remove duplicate images
-        self.data_frame = self.remove_duplicate_images(self.data_frame)
-        
-        self.data_frame.reset_index(drop=True, inplace=True)  # Reset index to avoid any out-of-bounds issues
+
+        self.data_frame.reset_index(drop=True, inplace=True)
 
     def __len__(self):
         return len(self.data_frame)
@@ -45,7 +49,13 @@ class ImageDataset(Dataset):
     
     def is_valid_image(self, pixel_string):
         pixels = np.array(pixel_string.split(), dtype=np.uint8)
-        return np.any(pixels > 0)  # Check if any pixel is not black
+        return np.any(pixels > 0)
+
+    def contains_face(self, pixel_string):
+        pixels = np.array(pixel_string.split(), dtype=np.uint8).reshape(48, 48)
+        face_locations_hog = face_recognition.face_locations(pixels, model="hog")
+        face_locations_cnn = face_recognition.face_locations(pixels, model="cnn")
+        return len(face_locations_hog) > 0 or len(face_locations_cnn) > 0
 
     def remove_duplicate_images(self, data_frame):
         seen_hashes = set()
@@ -61,3 +71,4 @@ class ImageDataset(Dataset):
                 unique_rows.append(row)
         
         return pd.DataFrame(unique_rows)
+
