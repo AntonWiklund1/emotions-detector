b23b30bd40d1b75ee30a5f2b87cd3747af33cd15
AntonWiklund1 <awiklund76@gmail.com>
2024-05-31 18:34:23 +0300

    feat: Add utils/__init__.py module with various utility functions

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from .checkpoint import get_checkpoint
+from .log import log
+from .folds import create_folds
+from .pre_trained import load_pretrained_weights
+from .CustomLRFinder import CustomLRFinder
